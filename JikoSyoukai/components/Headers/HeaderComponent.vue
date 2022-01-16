5f369d7d2ef5338d1201507d8677e8972162567c
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-16 21:01:20 +0900

    Top

---
+++ JikoSyoukai/components/Headers/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>
+      <a href="">Fujiwara Takumi ポートフォリオ</a>
+    </h1>
+    <nav>
+      <ul>
+        <li><a href="">ポートフォリオ</a></li>
+        <li><a href="">私について</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 30px 4% 10px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  display:flex;
+  align-items: center;
+  background-color: #fff;
+  align-items: center;
+  list-style: none;
+}
+ul {
+    list-style: none;
+    margin: 0;
+    display: flex;
+}
+li{
+  margin: 0 0 0 15px;
+  font-size: 14px;
+}
+</style>
---
+++ JikoSyoukai/pages/Top.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <headers-header-component>
+      </headers-header-component>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "../components/Headers/HeaderComponent.vue";
+export default {
+  components: {
+    HeaderComponent
+  }
+}
+</script>
