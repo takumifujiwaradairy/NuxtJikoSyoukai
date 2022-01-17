43f3ce8f80d1cbc4961813273e3f3e48a64ff628
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-17 14:06:13 +0900

    Todolist

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="container">
     <h1>Todo App</h1>
-    <p><input type="text" name="content" v-model="content"></p>
+    <p><input type="text" name="content" v-model="content"  @focus="set_flg"/></p>
     <div>
       <button @click="insert">save</button>
-      <button>find</button>
+      <button @click="find">find</button>
     </div>
     <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        <span>{{ todo.content }}</span><span>({{ todo.created }})</span><span>×</span>
+      <li v-for="(todo, index) in display_todos" :key="index">
+        <span>{{ todo.content }}</span><span>({{ todo.created }})</span><span @click="remove(todo)">×</span>
       </li>
     </ul>
   </section>
@@ -20,16 +20,43 @@ import {mapState} from 'vuex';
 export default {
   data: function() {
     return {
-      content: ''
+      content: '',
+      find_flg: false
     }
   },
   computed: {
-    ...mapState(['todos'])
+    ...mapState(['todos']),
+    display_todos: function() {
+      if(this.find_flg) {
+        var arr = [];
+        var data = this.todos;
+        data.forEach(element => {
+          if(element.content.toLowerCase() == this.content.toLowerCase()) {
+            arr.push(element);
+          }
+        });
+        return arr;
+      } else {
+        return this.todos;
+      }
+    }
   },
-  methods:{
+  methods: {
     insert: function() {
-      this.$store.commit('insert',{content: this.content});
+      this.$store.commit('insert', {content: this.content});
       this.content = '';
+    },
+    find: function() {
+      this.find_flg = true;
+    },
+    set_flg: function() {
+      if(this.find_flg) {
+        this.find_flg = false;
+        this.content = '';
+      }
+    },
+    remove: function(todo) {
+      this.$store.commit('remove', todo)
     }
   }
 }
